8ebdef5f10cdc364beea5e4212977c16ef8f39f4
SHIMA Masahiro <masahiro.shima@nifty.com>
2024-05-23 04:45:16 +0000

    modefied

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 import React, { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
@@ -12,7 +12,7 @@ function App() {
   useEffect(() => {
     const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
-      error: (err) => console.error('Error observing todos:', err),
+      error: (err) => console.error("Error observing todos:", err),
     });
 
     return () => subscription.unsubscribe();
@@ -21,25 +21,26 @@ function App() {
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
       .then(() => {
-        setTodos(todos.filter(todo => todo.id !== id));
+        setTodos(todos.filter((todo) => todo.id !== id));
       })
-      .catch(err => console.error('Error deleting todo:', err));
+      .catch((err) => console.error("Error deleting todo:", err));
   }
 
   function createTodo() {
     const content = window.prompt("Todo content");
     if (content) {
       client.models.Todo.create({ content })
-        .then((newTodo) => {
-          setTodos([...todos, newTodo]);
+        .then((response) => {
+          const newTodo = response as unknown as Schema["Todo"]["type"];
+          setTodos((prevTodos) => [...prevTodos, newTodo]);
         })
-        .catch(err => console.error('Error creating todo:', err));
+        .catch((err) => console.error("Error creating todo:", err));
     }
   }
 
   return (
     <Authenticator>
-      {({ signOut, user }) => (
+      {({ signOut }) => (
         <main>
           <button onClick={signOut}>Sign out</button>
           <h1>My todos</h1>
@@ -51,9 +52,7 @@ function App() {
               </li>
             ))}
           </ul>
-          <div>
-            項目名をクリックするとその項目が削除されます
-          </div>
+          <div>項目名をクリックするとその項目が削除されます</div>
         </main>
       )}
     </Authenticator>
